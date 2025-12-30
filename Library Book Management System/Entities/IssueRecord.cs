f0581d9f3645eddac17da8260f2a585bab693b59
sharath chary <sharathchary3568@outlook.com>
2025-12-30 22:22:58 +0530

    Fields, Methods and Properties created in Members.cs and IssueRecord.cs

---
+++ Library Book Management System/Entities/IssueRecord.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_Book_Management_System.Entities
+{
+    public class IssueRecord
+    {
+        public string? IssueID { get; private set; }
+        public string? BookID { get; private set; }
+        public string? MemberID { get; private set; }
+        public DateTime IssueDate { get; private set; }
+        public DateTime? ReturnDate { get; private set; }
+        public bool IsReturned { get; private set; }
+
+        public IssueRecord(string bookID, string memberID)
+        {
+            BookID = bookID;
+            MemberID = memberID;
+            IssueDate = DateTime.Now;
+            ReturnDate = null;
+            IsReturned = false;
+        }
+
+        internal void SetIssueID(string issueID)
+        {
+            IssueID = issueID;
+        }
+
+        public void MarkAsReturned()
+        {
+            if (IsReturned)
+                throw new InvalidOperationException("Book already returned.");
+
+            IsReturned = true;
+            ReturnDate = DateTime.Now;
+        }
+
+    }
+}
---
+++ Library Book Management System/Entities/Member.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_Book_Management_System.Entities
+{
+    public class Member
+    {
+        public string? MemberID { get; private set; }
+        public string? Name { get; private set; }
+        public int MaxAllowedBooks { get; private set; }
+        public int CurrentIssuedCount { get; private set; }
+        public Member (string name, int maxallowedbooks)
+        {
+            Name = name;
+            MaxAllowedBooks = maxallowedbooks;
+        }
+
+        internal void setMemberID(string memberID)
+        {
+            MemberID = memberID;
+        }
+
+        public bool CanIssueBook()
+        {
+            return CurrentIssuedCount < MaxAllowedBooks;
+        }
+
+        public void IncrementIssueCount()
+        {
+            if (!CanIssueBook())
+            {
+                throw new InvalidOperationException("Member has reached the maximum allowed issued books,");
+            }
+            CurrentIssuedCount++;
+        }
+
+        public void DecrementIssueCount()
+        {
+            if(CurrentIssuedCount <= 0)
+            {
+                throw new InvalidOperationException("No issued books to return");
+            }
+
+            CurrentIssuedCount--;
+        }
+    }
+}
